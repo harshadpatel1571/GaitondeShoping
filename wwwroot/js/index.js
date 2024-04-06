98f880cc2406eba706f5973ef308b910d8a1ce87
Janhvi Kalariya <janhvikalariya@gmail.com>
2024-04-06 15:45:35 +0530

    cart.html link

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -9,6 +9,7 @@ $(document).ready(async function () {
             if (result.data != null) {
                 $.each(result.data.slice(0, 6), function (index, value) {
                     let html = `<div class="col px-0">
+                    <a href="product-page.html?product_id=${value.product_id}" style="text-decoration: none;">
                 <div class="card rounded-0 border-1">
                     <img src="${value.images[0].image_url}" class="card-img-top rounded-0" alt="...">
                         <div class="card-footer border-0 rounded-0 bg-orange-20 d-flex align-items-center justify-content-between">
@@ -16,6 +17,7 @@ $(document).ready(async function () {
                             <img src="wwwroot/images/product-add-icon.svg" alt="add-icon">
                         </div>
                     </div>
+                </a>
                 </div>`;
                     $("#divProduct").append(html);
                 });
